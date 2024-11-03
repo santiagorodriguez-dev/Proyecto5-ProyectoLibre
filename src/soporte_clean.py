cd3fe186ffd9f61ae07a4361bafb517b8da377ed
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-11-03 20:19:15 +0100

    todos

--- src/soporte_clean.py
+++ src/soporte_clean.py
@@ -1,13 +1,13 @@
 import pandas as pd # type: ignore
 
-def merge_dataframe(df_peliculas_imdb, df_books_imdb, df_streaming_imdb):
+def merge_dataframe(df_peliculas, df_books, df_streaming):
 
-    df_peliculas_imdb['title'] = df_peliculas_imdb['title'].str.strip()
-    df_books_imdb['title'] = df_books_imdb['title'].str.strip()
-    df_streaming_imdb['title'] = df_streaming_imdb['title'].str.strip()
+    df_peliculas['title'] = df_peliculas['title'].str.strip()
+    df_books['title'] = df_books['title'].str.strip()
+    df_streaming['title'] = df_streaming['title'].str.strip()
 
-    df_merge = pd.merge(df_peliculas_imdb, df_books_imdb, how="left", on=["title"])
-    df_all = pd.merge(df_merge, df_streaming_imdb, how="left", on=["title"])
+    df_merge = pd.merge(df_peliculas, df_books, how="left", on=["title"])
+    df_all = pd.merge(df_merge, df_streaming, how="left", on=["title"])
 
     return df_all
 
@@ -16,5 +16,6 @@ def clean_data(df_all):
     df_all[['imdbId']] = df_all[['imdbId']].fillna(value='tt0000000')
     df_all[['list_plataform']] = df_all[['list_plataform']].fillna(value='')
     df_all[['releaseYear_y']] = df_all[['releaseYear_y']].fillna(value=0)
+    df_all[['rating_y','releaseYear_x']] = df_all[['rating_y','releaseYear_y']].fillna(value=0)
 
     return df_all
